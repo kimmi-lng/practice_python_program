ab73acc2ca61bd630a9737c2139da3033ade8a45
Kim Russel <kimrussel0718@gmail.com>
2026-03-09 18:28:11 +0800

    first i include to ask the user to input the two numbers, type cast the string into float, and add them to print its sum

--- sum_two_numbers.py
+++ sum_two_numbers.py
@@ -1,1 +1,4 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+number = float (input("Enter the first number: "))
+number += float (input("Enter the second number: "))
+print(number, "is the sum of this two numbers")
