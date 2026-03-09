f1dd385f86b8b86bd8a77210c5011a180ba4b5db
Kim Russel <kimrussel0718@gmail.com>
2026-03-09 19:27:41 +0800

    i change my code into for loop so that i can continously ask the user with the 10 number and then sum and print the result with f method

--- sum_all_numbers.py
+++ sum_all_numbers.py
@@ -1,2 +1,7 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
-numbers = float (input("Enter 10 numbers: "))
+all_numbers = []
+for i in range(10):
+    numbers = float (input(f"Enter number {i+1}: "))
+    all_numbers.append(numbers)
+total = sum(all_numbers)
+print(f"The sum of all 10 numbers is: {total}")
