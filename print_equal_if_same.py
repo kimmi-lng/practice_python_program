b2049bc27054bd51ec22865e6fe2b7f16638cd5c
Kim Russel <kimrussel0718@gmail.com>
2026-03-09 18:12:19 +0800

    asking for two numbers

---
+++ print_equal_if_same.py
@@ -0,0 +1,3 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+first_numb = float(input("Enter the first number: "))
+second_numb = float(input("Enter the second number: "))
