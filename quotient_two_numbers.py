8e98469b81035b2ebee72d54dcd6595cb8c04bf9
Kim Russel <kimrussel0718@gmail.com>
2026-03-09 18:45:47 +0800

    asked the users to input two number and the print its quotient with '/' not '//' because that is for floor division

--- quotient_two_numbers.py
+++ quotient_two_numbers.py
@@ -1,1 +1,6 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point
+
+number = int(input("Enter the first number: "))
+number /= float (input("Enter the second number: "))
+# use / for normal division with decimals not // because that is for floor division
+print(number, "is the quotient of this two numbers")
