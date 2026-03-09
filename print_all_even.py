3bdcde0f3e98bc65ce6c5ae6a8dd80a1ee018db5
Kim Russel <kimrussel0718@gmail.com>
2026-03-09 21:18:17 +0800

    here i added an empty list and append all the even numbers so that i can print the result in a list

--- print_all_even.py
+++ print_all_even.py
@@ -1,4 +1,10 @@
 #Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+all_even = []
+for numbers in range(0,101):
+    if numbers % 2 == 0:
+        all_even.append(numbers)
+print(all_even, "are all even number")
+
 for numbers in range(0,101):
     if numbers % 2 == 0:
         print(f"{numbers} is even")
