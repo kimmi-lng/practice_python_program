5460df5535b299f4c422f2253729ae7349b2869d
Kim Russel <kimrussel0718@gmail.com>
2026-03-09 19:03:43 +0800

    i finished the code bu assigning values into variables and print the result with f method

--- raised_second_number.py
+++ raised_second_number.py
@@ -1,4 +1,7 @@
 #Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 #first number will be the base
+base = float(input("Enter the first number (base): "))
 #second number will be the exponent
+exponent = float (input("Enter the second number (exponent): "))
 #print the result
+print(f"When {base} is raised to the power is {exponent} the result is {base ** exponent}")
