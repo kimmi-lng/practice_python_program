3efeeaa2af3f3547e69f3898083e1e95db3d56c3
Kim Russel <kimrussel0718@gmail.com>
2026-03-09 18:59:55 +0800

    started program six by commmenting which is which

---
+++ raised_second_number.py
@@ -0,0 +1,4 @@
+#Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+#first number will be the base
+#second number will be the exponent
+#print the result
