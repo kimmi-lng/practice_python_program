773abe91df8aced9a9695a5e7682798f4a5b425a
Kim Russel <kimrussel0718@gmail.com>
2026-03-09 17:52:46 +0800

    assigned variable name and type cast int to float

--- print_bigger_number.py
+++ print_bigger_number.py
@@ -1,3 +1,3 @@
 # Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
-input("Enter the first number: ")
-input("Enter the second number: ")
+first_numb = float(input("Enter the first number: "))
+second_numb = float(input("Enter the second number: "))
