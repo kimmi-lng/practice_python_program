0bb7691138385691d8330a6d9471c4f1977198ff
Kim Russel <kimrussel0718@gmail.com>
2026-03-09 20:29:42 +0800

    in for loop i ask the user to input 10 numbers in range and then append that number if its odd then i printed how many are odd in those numbers

--- how_many_are_odd.py
+++ how_many_are_odd.py
@@ -1,4 +1,10 @@
 #Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 #ask for the numbers in a loop
+all_numbers = []
+for i in range(10):
+    numbers = float(input(f"Enter the number {i+1}: "))
 #check how many are ood numbers
+    if numbers%2 != 0:
+        all_numbers.append(numbers)
 #print the result
+print(f"{len(all_numbers)}x is/are odd numbers")
