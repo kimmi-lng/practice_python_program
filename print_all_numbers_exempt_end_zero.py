3f8a53f567feefa93b78c1b6ae4cab8a0976d828
Kim Russel <kimrussel0718@gmail.com>
2026-03-09 21:21:45 +0800

    commented what is needed

---
+++ print_all_numbers_exempt_end_zero.py
@@ -0,0 +1,4 @@
+#Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+#create an empty list
+#identify all the needs
+#print the result in a list and also by line
