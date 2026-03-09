4effce23f760a440701d677557edde3f8cfc1467
Kim Russel <kimrussel0718@gmail.com>
2026-03-09 18:21:23 +0800

    finished the program by setting condition thaat prints if the two numbers are the same

--- print_equal_if_same.py
+++ print_equal_if_same.py
@@ -1,3 +1,7 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 first_numb = float(input("Enter the first number: "))
 second_numb = float(input("Enter the second number: "))
+if first_numb == second_numb:
+    print("Equal")
+else:
+    print("Not Equal")
