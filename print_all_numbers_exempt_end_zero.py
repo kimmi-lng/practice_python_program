b0500dd994fe535d95ee8885f8de0e0134e8d761
Kim Russel <kimrussel0718@gmail.com>
2026-03-09 21:29:44 +0800

    just followed the comments i made, set the conditions, and pint the result in a list and in ascending order that is by lines

--- print_all_numbers_exempt_end_zero.py
+++ print_all_numbers_exempt_end_zero.py
@@ -1,4 +1,12 @@
 #Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 #create an empty list
+all_numbers = []
 #identify all the needs
-#print the result in a list and also by line
+for numbers in range(0,101):
+    if numbers % 10 != 0:
+        all_numbers.append(numbers)
+#in a list
+print("numbers that doesn't end with 0 are:", all_numbers)
+#ascending
+for numbers in all_numbers:
+    print(numbers)
