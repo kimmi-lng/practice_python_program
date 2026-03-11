cbe36419067b444946f66a8a2ca3d6ddf200904b
Kim Russel <kimrussel0718@gmail.com>
2026-03-11 11:38:28 +0800

    added comments for the code flow

---
+++ reminder_two_numbers.py
@@ -0,0 +1,4 @@
+#Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
+#ask for two number
+#check the reminder
+#print the result
