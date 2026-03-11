d559f75e538627a66af9c5ca8f647957f66227ef
Kim Russel <kimrussel0718@gmail.com>
2026-03-11 11:36:45 +0800

    added where i ask the users to input two numbers then set condition then prints the result

--- floor_division_two_numbers.py
+++ floor_division_two_numbers.py
@@ -1,4 +1,8 @@
 #Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 #ask for two numbers
+numb1 = float(input("Enter the first number: "))
+numb2 = float(input("Enter the second number: "))
 #use the operation // to not include the decimal
+result = numb1 // numb2
 #print the result
+print(f"The result of the division of this two numbers is {result}")
