04074013bf7c292096af28536acb96dc8ec18ca8
Kim Russel <kimrussel0718@gmail.com>
2026-03-11 11:19:37 +0800

    asked the users to input two numbers, compare both and then print the smaller number

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -1,4 +1,10 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
 #ask for 2 numbers
+numb1 = float(input("Enter the first number: "))
+numb2 = float(input("Enter the second number: "))
 #condition/compare
+if numb1 < numb2:
 #print the smaller number
+    print(numb1, "is smaller than", numb2)
+else:
+    print(numb2, "is smaller than", numb1)
