00dcba704692bdd997b600697f87fc32b51a8f97
Kim Russel <kimrussel0718@gmail.com>
2026-03-11 11:33:36 +0800

    cemmented the flow of the code

---
+++ floor_division_two_numbers.py
@@ -0,0 +1,4 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+#ask for two numbers
+#use the operation // to not include the decimal
+#print the result
