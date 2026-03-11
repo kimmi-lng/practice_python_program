f7c71981aeb867e9c9bb55f3398ac074fa63918b
Kim Russel <kimrussel0718@gmail.com>
2026-03-11 11:27:09 +0800

    added first where i ask the user to input two numbers then cemmented what is next

---
+++ difference_of_two_numbers.py
@@ -0,0 +1,4 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+numb1 = float(input("Enter the first number: "))
+numb2 = float(input("Enter the second number: "))
+#condition and result
