2182d5d98c7fdd68c434962743fda106b2c82cc0
Kim Russel <kimrussel0718@gmail.com>
2026-03-11 11:52:47 +0800

    added the condition where i can print the remainder of two numbers then printed its results

--- reminder_two_numbers.py
+++ reminder_two_numbers.py
@@ -1,4 +1,8 @@
 #Prog05: Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
 #ask for two number
+numb1 = float(input("Enter the first number: "))
+numb2 = float(input("Enter the second number: "))
 #check the reminder
+result = numb1 % numb2
+print(f"The remainder of this two number is {result}")
 #print the result
