e096d6a19e75cd101b72df87f932b2dd239da066
Kim Russel <kimrussel0718@gmail.com>
2026-03-11 11:24:51 +0800

    asked for two numbers then add the condition that if two numbers are not equal then it will print a result not equal

--- print_not_equal.py
+++ print_not_equal.py
@@ -1,4 +1,8 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 #ask for 2 numbers
+numb1 = float(input("Enter number 1: "))
+numb2 = float(input("Enter number 2: "))
 #condition
+if numb1 != numb2:
 #print result
+    print("not equal")
