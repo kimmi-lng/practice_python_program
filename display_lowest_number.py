4ba28867d47059ee2311c1a29d7db0199aa29d96
Kim Russel <kimrussel0718@gmail.com>
2026-03-19 21:00:02 +0800

    added flow to follow

---
+++ display_lowest_number.py
@@ -0,0 +1,4 @@
+#Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
+#empty set maybe
+#continously ask for number
+#set condition maybe try and exempt
