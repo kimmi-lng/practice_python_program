fd7233e4ab8aaa28c466aa2ab5115fb54974f700
Kim Russel <kimrussel0718@gmail.com>
2026-03-19 13:18:52 +0800

    just follow the flow starded but asking for numbers setting condition and store them if they are even numbers then used len function to count how many are even

--- how_many_even.py
+++ how_many_even.py
@@ -1,5 +1,11 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
 #stores even numbers
+all_even_numbers = []
 #ask for 10 numbers
+for i in range(10):
+    numbers = float(input(f"Enter the number {i+1}: "))
 #condition
+    if numbers % 2 == 0:
+        all_even_numbers.append(numbers)
 #print how many are even numbers
+print(len(all_even_numbers), "is/are even number/s.")
