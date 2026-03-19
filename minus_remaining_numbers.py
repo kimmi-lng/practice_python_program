fbffa827ea5db5ee5a4e985deb45a20fb7922093
Kim Russel <kimrussel0718@gmail.com>
2026-03-19 12:46:31 +0800

    commented the steps ill take

---
+++ minus_remaining_numbers.py
@@ -0,0 +1,4 @@
+#Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+#ask for the numbers
+#condition
+#print the result
