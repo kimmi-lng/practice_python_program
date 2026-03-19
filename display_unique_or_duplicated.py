083e72508e0b2049340343b2523a5fe3859cd878
Kim Russel <kimrussel0718@gmail.com>
2026-03-19 20:47:12 +0800

    used while true to continously ask for number then print if its uniqu or if it is duplicated then print all the unique numbers once the condition is broken

--- display_unique_or_duplicated.py
+++ display_unique_or_duplicated.py
@@ -1,6 +1,16 @@
 #Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
 #empty list
+unique_numbers = []
 #ask for numbers continously
-#print unique if numbers dont have equal value
+while True:
+    ask_numb = float(input("Enter a number: "))
 #print duplicate if it has equal value
+    if ask_numb in unique_numbers:
+        print(f"The number {ask_numb} is duplicated")
+        break
+#print unique if numbers dont have equal value
+    else:
+        unique_numbers.append(ask_numb)
+        print(f"The number {ask_numb} is unique")
 #then print the list?
+print("The list of all unique numbers: ",unique_numbers)
