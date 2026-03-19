a2889b031a068ffb0a3a6bf14511a0bb198ee02d
Kim Russel <kimrussel0718@gmail.com>
2026-03-19 22:01:36 +0800

    listed flow to follow

---
+++ display_lowest_to_highest.py
@@ -0,0 +1,4 @@
+#Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
+#empty list
+#ask for numbers
+#condition maybe try and exempt for invalid input
