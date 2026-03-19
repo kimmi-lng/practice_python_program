15dfff004a53d1e07c1a118b1275d6c039d8d55a
Kim Russel <kimrussel0718@gmail.com>
2026-03-19 18:32:51 +0800

    i started to input an empty list then store there the 10 numbers placed by user, then after i added the condition .count() where is checks the occurence of the numbers

--- all_numbers_no_duplicates.py
+++ all_numbers_no_duplicates.py
@@ -1,5 +1,12 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+numbers = []
 #ask for numbers
+for i in range(10):
+    numb = float(input(f"Enter number {i+1}: "))
 #condition
+    numbers.append(numb)
+for vals in numbers:
+    if numbers.count(vals) == 1:
 #add also maybe an empty list
 #print results
+        print(vals)
