ffbf49e210b029bd86c6c152fa52bb9161d124ea
Kim Russel <kimrussel0718@gmail.com>
2026-03-19 20:29:49 +0800

    commented the flow to follow

---
+++ display_unique_or_duplicated.py
@@ -0,0 +1,6 @@
+#Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display "Unique" after input when the inputted number don't have duplicate. Display "Duplicate" after input when the inputted number have duplicate.
+#empty list
+#ask for numbers continously
+#print unique if numbers dont have equal value
+#print duplicate if it has equal value
+#then print the list?
