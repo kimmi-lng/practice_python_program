876e4dc9887929fcdb4b26f976b1d072599c69f5
Kim Russel <kimrussel0718@gmail.com>
2026-03-19 13:00:11 +0800

    asked for 10 numbers and set condition to subract the rest of the numbers from the first numbers

--- minus_remaining_numbers.py
+++ minus_remaining_numbers.py
@@ -1,4 +1,10 @@
 #Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 #ask for the numbers
-#condition
-#print the result
+all_numbers = []
+for i in range(10):
+    numbers = float(input(f"Enter the number {i+1}: "))
+    all_numbers.append(numbers)
+
+difference = all_numbers[0] - sum(all_numbers[1:10])
+
+print(f"The difference of {all_numbers[0]} and other numbers is {difference}")
