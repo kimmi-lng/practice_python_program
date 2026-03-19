d173f89a674545aea4654991e8e1996558a78379
Kim Russel <kimrussel0718@gmail.com>
2026-03-19 13:14:46 +0800

    set code flow

---
+++ how_many_even.py
@@ -0,0 +1,5 @@
+#Prog07: Create a program that ask user to input 10 numbers. Print how many are even numbers.
+#stores even numbers
+#ask for 10 numbers
+#condition
+#print how many are even numbers
