94126db98a3a96094d66049ceae00c56ff63e14e
Kim Russel <kimrussel0718@gmail.com>
2026-03-19 22:15:27 +0800

    used try and except to identify which is invalid from input and till that, the code will continously ask for a number then will sort them from lowest to highest

--- display_lowest_to_highest.py
+++ display_lowest_to_highest.py
@@ -1,4 +1,13 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
 #empty list
+numbers = []
 #ask for numbers
+while True:
+    try:
+        ask_numb = float(input("Enter a number: "))
+        numbers.append(ask_numb)
+    except ValueError:
+        print("Invalid input")
+        break
+print(f"From lowest to highest {sorted(numbers)}")
 #condition maybe try and exempt for invalid input
