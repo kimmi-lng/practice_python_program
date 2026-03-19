0bff6b75db97f24c4e300e01cb38d820095984e9
Kim Russel <kimrussel0718@gmail.com>
2026-03-19 14:01:10 +0800

    follows the comments and then set conditions and prints the results

--- print_numbers_between_two_numbers.py
+++ print_numbers_between_two_numbers.py
@@ -1,5 +1,11 @@
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 #create an empty list
+numbers_between = []
 #ask for two numbers
+number1 = int(input("Enter number 1: "))
+number2 = int(input("Enter number 2: "))
 #set conditions
+for i in range(number1 + 1, number2):
+    numbers_between.append(i)
 #print the result
+print(numbers_between)
