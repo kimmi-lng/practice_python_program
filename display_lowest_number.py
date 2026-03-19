ce5bc7d1fda99c3dd737ec09f21c5c84ea1e904a
Kim Russel <kimrussel0718@gmail.com>
2026-03-19 21:07:24 +0800

    added an empty set to be able to use min() funtion to find the lowest from all the numbers that was entered by user if and only if the condition is broken

--- display_lowest_number.py
+++ display_lowest_number.py
@@ -1,4 +1,13 @@
 #Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 #empty set maybe
+numb_list = []
 #continously ask for number
+while True:
 #set condition maybe try and exempt
+    try:
+        ask_numb = float(input("Enter a number: "))
+        numb_list.append(ask_numb)
+    except ValueError:
+        print("Invalid input")
+        break
+print(f"The lowest number is {min(numb_list)}")
