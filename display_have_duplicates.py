7828a094c142e96ecf17f61da53c3c9d8891f4ea
Kim Russel <kimrussel0718@gmail.com>
2026-03-19 22:44:46 +0800

    started with an empty set then set contions where i ask user for 10 numbers using for loop then append it on a list then after i created another empty list to store all vals that has duplicates using the count funtion then print the result

--- display_have_duplicates.py
+++ display_have_duplicates.py
@@ -1,5 +1,13 @@
 #Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 #empty list
+numbers = []
+duplicated_numbers = []
 #ask for numbers
-#condition
+for i in range(10):
+    ask_number = float(input(f"Enter a number {i+1}: "))
+    numbers.append(ask_number)
+for vals in numbers:
+    if numbers.count(vals) > 1:
+        duplicated_numbers.append(vals)
 #print result
+print("Numbers that has duplicate: ", duplicated_numbers)
