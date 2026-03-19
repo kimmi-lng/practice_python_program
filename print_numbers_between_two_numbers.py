d7b7aa6baec9adb41a899bb3fd7ecfc84a963ba3
Kim Russel <kimrussel0718@gmail.com>
2026-03-19 13:57:27 +0800

    added comments

---
+++ print_numbers_between_two_numbers.py
@@ -0,0 +1,5 @@
+#Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+#create an empty list
+#ask for two numbers
+#set conditions
+#print the result
