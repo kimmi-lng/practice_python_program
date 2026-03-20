476ae8ead6a5026c32d6a362d85ae77325474f67
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 21:05:05 +0800

    ask for the full name and the prefix that needs to be removed then set conditions then print the result

--- remove_prefix.py
+++ remove_prefix.py
@@ -1,7 +1,14 @@
 #Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
 #ask for the full name with the prefix
+ask_name = input("Please enter your name with the prefix: ")
 #ask for the prefix that needs to be removed
+prefix = input("Please enter your prefix that needs to be removed: ")
 #condition
+if ask_name.startswith(prefix):
+    ask_name = ask_name[len(prefix):]
+else:
+    ask_name = ask_name
 #print result
+print(ask_name)
 
 
