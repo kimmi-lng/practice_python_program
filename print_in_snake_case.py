3fbfa5dbfc564d05cdfc604ae7e8e3a5e0d751ec
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 18:21:03 +0800

    used the lower() and replace() to make all case lower and replace the space characters with the _ for snake case

--- print_in_snake_case.py
+++ print_in_snake_case.py
@@ -3,3 +3,9 @@
 #Input: jUAn DEla CrUZ
 #Output: juan_dela_cruz
 #use lower() to make all case snall then use replace() to replace space characters
+ask_name = input("Enter your full name in incorrect casing: ")
+snake_case = ask_name.lower().replace(" ", "_")
+if ask_name.strip():
+    print(f"The entered name in snake case is: {snake_case}")
+else:
+    print("Invalid input, must be somthing")
