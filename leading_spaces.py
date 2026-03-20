3441eb2f64f2ec18184d29c4c196874da4dcc6a5
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 18:36:26 +0800

    commented a flow to follow

---
+++ leading_spaces.py
@@ -0,0 +1,5 @@
+#Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+#ask user for the name with spaces before it
+#set condition to find the first character
+#use that index to get the range of that first string
+#print result
