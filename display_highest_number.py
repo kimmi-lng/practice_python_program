e54a6d2afde9cf3e2a6c94605c65dc43676a34f8
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 13:30:51 +0800

    set a flow to follow

---
+++ display_highest_number.py
@@ -0,0 +1,4 @@
+#Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
+#empty list
+#use try and except to ask for numbers and to set condition
+#print the result
