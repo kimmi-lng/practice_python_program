e94a272c05e8a41c8f416e8eb38dded724f0f554
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 15:13:07 +0800

    set flow

---
+++ print_all_capital.py
@@ -0,0 +1,5 @@
+#Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+#Example:
+#Input: Juan Dela Cruz
+#Output: JUAN DELA CRUZ
+#use try and except again maybe to differentiate values
