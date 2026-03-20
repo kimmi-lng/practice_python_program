c9e502c6c54a2fb9038248f60bf1a401429814b2
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 18:46:40 +0800

    added inputs for the user to put their name then used while loop to identify the inext of the first character when there is a leading spaces then use thats index to print the characters or the strings

--- leading_spaces.py
+++ leading_spaces.py
@@ -1,5 +1,14 @@
 #Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+index = 0
 #ask user for the name with spaces before it
+ask_name = input("Please enter your name with spaces before it: ")
+
+while index < len(ask_name) and ask_name[index] == " ":
+    index += 1
+
+first_character = ask_name[index:]
+print(first_character)
+
 #set condition to find the first character
 #use that index to get the range of that first string
 #print result
