1d0cb1eaafd28bb131492c3fca8c49440e5b1b7d
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 15:38:27 +0800

    used if else to check if it is empty or not then used lower() to transfom capitalized string to their lower case

--- print_all_lower.py
+++ print_all_lower.py
@@ -3,5 +3,11 @@
 #Input: Juan Dela Cruz
 #Output: juan dela cruz
 #ask for users name
+ask_name = input("Enter your full name name: ")
 #condition to check if its empty of what
+if ask_name.strip():
+    lowered_case = ask_name.lower()
 #print
+    print(f"The entered name in all lower case: {lowered_case}")
+else:
+    print("Invalid input, cannot be empty")
