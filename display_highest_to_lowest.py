92451aaeec86a907dca0f3f5c859bd68d690e288
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 13:39:26 +0800

    commented the flow to follow

---
+++ display_highest_to_lowest.py
@@ -0,0 +1,4 @@
+#Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
+#an empty list to be sorted
+#add condition use while True then try and except to identify value
+#print the result from highest to lowest
