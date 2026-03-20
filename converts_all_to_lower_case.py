f7b70f93ca30cd82ad8f6287c7aa747ca490d3c3
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 23:00:51 +0800

    commented a flow to follow

---
+++ converts_all_to_lower_case.py
@@ -0,0 +1,4 @@
+#Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+#ask for a string
+#then convert those capitalized string using its ascii values
+#print results
