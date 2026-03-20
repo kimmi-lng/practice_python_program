d0c9f7cf3c6114f7795eb34bbc6b9b9be02f047f
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 18:06:36 +0800

    commented a flow to follow

---
+++ print_pascal_case.py
@@ -0,0 +1,5 @@
+#Prog09: Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+#Example:
+#Input: jUAn DEla CrUZ
+#Output: JuanDelaCruz
+#first casing need to be fixed then maybe replace() to replace space characters
