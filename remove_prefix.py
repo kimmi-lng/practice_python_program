59147f49c655d753d8a89786eb369e476cc96e10
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 21:01:32 +0800

    commented the flow to be followed

---
+++ remove_prefix.py
@@ -0,0 +1,7 @@
+#Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+#ask for the full name with the prefix
+#ask for the prefix that needs to be removed
+#condition
+#print result
+
+
