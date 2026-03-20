9ef691ed1ad830111d11ee57cd53ef5a10ea88ad
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 13:28:06 +0800

    used while loop to ask for numbers continously once theres an invalid input the conditions will take place and look for most duplicated number then display it

--- display_most_diplicate.py
+++ display_most_diplicate.py
@@ -1,4 +1,14 @@
 #Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 #empty list
+list_numbers = []
 #condition try and exept maybe to continously ask for numbers then
+while True:
+    try:
+        ask_numb = float(input("Enter a number: "))
+        list_numbers.append(ask_numb)
+    except ValueError:
+        most_dup_number = max((list_numbers), key=list_numbers.count)
+        print ("Invalid input")
 #print result
+        print("Numbers that has most duplicate: ", most_dup_number)
+        break
