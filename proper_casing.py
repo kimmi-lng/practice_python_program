2c41bac382baa68c2595eb7ca90f4f211139398d
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 15:59:30 +0800

    first ask the user ti input their name then using title() we can implement proper casing then print result

--- proper_casing.py
+++ proper_casing.py
@@ -3,3 +3,9 @@
 #Input: jUAn DEla CrUZ
 #Output: Juan Dela Cruz
 #ask for the name then use if else statement for condition then print
+ask_name = input("Enter your full name: ")
+if ask_name.strip():
+    proper_casing = ask_name.title()
+    print(f"The entered name in proper casing: {proper_casing}")
+else:
+    print("Invalid input, cannot be empty")
