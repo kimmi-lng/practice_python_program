321d4b16114620b46a530ceb7c7351a3007d78ab
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 15:48:04 +0800

    commented a flow to follow

---
+++ proper_casing.py
@@ -0,0 +1,5 @@
+#Prog05: Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+#Example:
+#Input: jUAn DEla CrUZ
+#Output: Juan Dela Cruz
+#ask for the name then use if else statement for condition then print
