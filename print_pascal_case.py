0b1ec6945c928dd9013a1b0e422dd2dd2b4a7649
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 18:11:36 +0800

    followed the comments, ask for the name, check if there is to check then used title() to arrange in correct capitalization then replace() to replace space characters

--- print_pascal_case.py
+++ print_pascal_case.py
@@ -3,3 +3,9 @@
 #Input: jUAn DEla CrUZ
 #Output: JuanDelaCruz
 #first casing need to be fixed then maybe replace() to replace space characters
+ask_name = input("Enter your full name in incorrect casing: ")
+pascal_case = ask_name.title().replace(" ", "")
+if pascal_case.strip():
+    print(f"The entered name in pascal case is: {pascal_case}")
+else:
+    print("Invalid input, must be something")
