cac6c888ad621b9afcb9f07da6cbe9fd9b34987f
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 23:35:37 +0800

    added a comments to follow

---
+++ check_if_upper_case.py
@@ -0,0 +1,5 @@
+#Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+#first ask for a statement or a string
+#check these string if all are in upper case if it is it returns true
+#check if if in lower case then it returns false
+#print results
