1ff6e25cc4cfb16cb610309ce6ad609b9e5ba5f5
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 13:56:06 +0800

    commented a flow to follow

---
+++ display_the_average.py
@@ -0,0 +1,6 @@
+#Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
+#an empty list
+#condition using while true and also try and except
+#print the result
+
+
