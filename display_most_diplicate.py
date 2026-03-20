cb59b54604e16ac0ef893ef4dd3c7808e541465d
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 13:21:34 +0800

    commented the flow to follow

---
+++ display_most_diplicate.py
@@ -0,0 +1,4 @@
+#Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
+#empty list
+#condition try and exept maybe to continously ask for numbers then
+#print result
