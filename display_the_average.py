d33f6521064c49979fea0da2165ffef634f86c6f
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 13:59:47 +0800

    used while true to continously ask fo numbers, used try and except to differentiate values, once the condition is met the average will be taken then print the result. to get the average i used the sum() and len() functions

--- display_the_average.py
+++ display_the_average.py
@@ -1,6 +1,16 @@
 #Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 #an empty list
+list_numbs = []
 #condition using while true and also try and except
+while True:
+    try:
+        ask_numb = float(input("Enter a number: "))
+        list_numbs.append(ask_numb)
+    except ValueError:
+        print("Invalid input")
+        break
+average = sum(list_numbs) / len(list_numbs)
 #print the result
+print(f"The average is: {average}")
 
 
