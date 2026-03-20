7fbb41bfb84fae04df22b6b4e2ccf748f14716e7
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 23:20:55 +0800

    followed the flow, first ask for the string then check if these string are upper or lowers and if the condition are met it will be converted or remains as is, then prints the result

--- converts_all_to_lower_case.py
+++ converts_all_to_lower_case.py
@@ -1,4 +1,13 @@
 #Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 #ask for a string
+ask_statement = input("Please enter your statement/words to convert capitals to lowercase: ")
+lowered_case = ""
 #then convert those capitalized string using its ascii values
+for i in ask_statement:
+    if 'A' <= i <= 'Z':
+        lowered_char = chr(ord(i) + 32)
+        lowered_case += lowered_char
+    else:
+        lowered_case += i
 #print results
+print(f"Lowered case: {lowered_case}")
