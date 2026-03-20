647e0d3a372f779516c972bc402b35a7881adea4
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 16:19:50 +0800

    to count the words in the statement i simply used the lenfunction, with split function

--- print_number_of_words.py
+++ print_number_of_words.py
@@ -3,3 +3,9 @@
 #Input: We will weather the weather whatever the weather whether we like it or not
 #Output: 14
 #use count() maybe i think lemme check
+ask_statement = input("Enter a statement: ")
+if ask_statement.strip():
+    word_count = len(ask_statement.split())
+    print(f"The number of word in the statement: {word_count}")
+else:
+    print("Invalid input, cannot be empty")
