19060433e0733fc46b5a7e31de5a13cbfbefad1c
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 15:24:32 +0800

    commented a flow to follow

---
+++ print_all_lower.py
@@ -0,0 +1,7 @@
+#Prog04: Create a program that ask the user to input their fullname. Print the input in all lower case.
+#Example:
+#Input: Juan Dela Cruz
+#Output: juan dela cruz
+#ask for users name
+#condition to check if its empty of what
+#print
