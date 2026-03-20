14f891c39fc43a9719305d7b16751ea395c6636d
Kim Russel <kimrussel0718@gmail.com>
2026-03-20 13:35:14 +0800

    used while loop to continously ask for numbers then used try and except to identify the value of the data that was inputed by the user, once it is invalid input the highest number will be displayed

--- display_highest_number.py
+++ display_highest_number.py
@@ -1,4 +1,13 @@
 #Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number
 #empty list
+list_numbs = []
 #use try and except to ask for numbers and to set condition
+while True:
+    try:
+        ask_numb = float(input("Enter a number: "))
+        list_numbs.append(ask_numb)
+    except ValueError:
+        print("Invalid input")
+        break
 #print the result
+print(f"The highest number is: {max(list_numbs)}")
