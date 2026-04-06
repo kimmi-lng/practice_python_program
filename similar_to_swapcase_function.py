7c1edd367871333ac9b06305b7fd563094a5d115
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 16:13:15 +0800

    added two variable next is the condition

--- similar_to_swapcase_function.py
+++ similar_to_swapcase_function.py
@@ -1,4 +1,6 @@
 #Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 #add two variable which is the text and the empty one for storing
+statement = "SaNa All BALIW meOw mEow"
+swap_case = ""
 #add else if condition to check whether it is in upper case or lower case vise versa
 #print the result
