0ae1c9b59d9cadd10ee35401022ab32aa17fddd5
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 14:36:32 +0800

    added comments to follow

---
+++ similar_to_center_function.py
@@ -0,0 +1,4 @@
+#Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+#first is to set our variable
+#set condition for left and right spaces to center the text
+#print result
