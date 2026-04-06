95840daa032f8d2862f0439956a675a30c99f6d1
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 18:10:01 +0800

    followed the flow, started with the given text with space character in end or in right, i used while loop to continuously look for the first index of character in the end, then set conditions then print result

--- similar_to_rstrip_function.py
+++ similar_to_rstrip_function.py
@@ -1,9 +1,16 @@
 #Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
 #this function remove the space characters from right
 #first add a variable with stored sting statement or just a term with space at right part
+word = "Russel          "
 #we will start from last index to identify the location of the characters
+index = len(word) - 1
 #use while loop to look continuously
+while index >= 0 and word[index] == " ":
+    index -= 1
 #then use result as variable to store the result
+result = word[:index + 1]
 #print result
+print(f"Original: |{word}|")
+print(f"Result: |{result}|")
 
 
