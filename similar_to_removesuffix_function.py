bc6926be2897810bfae334093824e088ad044acb
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 18:46:11 +0800

    stored the data string into a variable

--- similar_to_removesuffix_function.py
+++ similar_to_removesuffix_function.py
@@ -1,5 +1,7 @@
 #Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
 #first is to define variable
-#get the lenght of the siffix to be removed
+comp_text_with_suffix = "Kim Russel jr"
+suffix = " jr"
+#get the length of the suffix to be removed
 #condition
 #print result
