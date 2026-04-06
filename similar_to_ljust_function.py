674c33a0ac4d9686c2b214749083857e914287b5
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 13:00:32 +0800

    instead of asking the user for the input and data, i used a fixed test to add spaces characters at right part of the text knowing that ljust() is for left justify, then i added another identifier ti push the condition or store, then print the result

--- similar_to_ljust_function.py
+++ similar_to_ljust_function.py
@@ -1,5 +1,10 @@
 #Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
-#time consuming if ill use the users input so maybe ill just add the fixed test
+#time consuming if ill use the users input so maybe ill just add the fixed text
+nickname = "Russel"
 #add or store the target width for the justify
+targ_width = 15
 #set condition
+subtract = max(0,targ_width - len(nickname))
+result = nickname + (subtract*" ")
 #print result
+print(f"Result: |{result}|, with total length of {len(result)}")
