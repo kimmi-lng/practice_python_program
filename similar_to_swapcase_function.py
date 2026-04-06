bfb0151f43a9e70bf96a71fba4cfb966018cf746
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 15:10:10 +0800

    commented a flow to follow

---
+++ similar_to_swapcase_function.py
@@ -0,0 +1,4 @@
+#Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+#add two variable which is the text and the empty one for storing
+#add else if condition to check whether it is in upper case or lower case vise versa
+#print the result
