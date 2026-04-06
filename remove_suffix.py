347bdb1ab3e9c558763e3252a9b4fa9f81e28c17
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 12:32:31 +0800

    instead of check i said removed and thats wrong so in the code i because endswith returns boolean so i fist ask the users to input the statement then let them input the suffix that needs to be checked, then i set the condition and first find the indext of the suffix to compare then print result

--- remove_suffix.py
+++ remove_suffix.py
@@ -1,5 +1,13 @@
 #Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 #users input also the suffix that needs to be removed
+statement = input("Enter a name/statement: ")
+suffix = input("Enter the suffix: ")
 #variable to store the suffix
+lenght_suffix = len(suffix)
 #condition
+if lenght_suffix > 0:
+    result = statement[-lenght_suffix:] == suffix
+else:
+    result = True
 #print result
+print(result)
