38779c4f8213dcf72923fcb4cd2b553072738307
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 14:45:20 +0800

    added the variable the is needed for the conditions, then i used if else to determine if the text can be centered or not, then print the result

--- similar_to_center_function.py
+++ similar_to_center_function.py
@@ -1,4 +1,14 @@
 #Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 #first is to set our variable
+nickname = "Russel"
+targ_width = 15
 #set condition for left and right spaces to center the text
+total_spaces = targ_width - len(nickname)
+if total_spaces > 0:
+    left_spaces = total_spaces // 2
+    right_spaces = total_spaces - left_spaces
+    result = (" " * left_spaces) + nickname + (" " * right_spaces)
+else:
+    result = nickname
 #print result
+print(f"Result: |{result}|")
