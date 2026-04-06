82baef711d2bdd2c9af08d46b91a545f4e212e19
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 18:38:47 +0800

    added a flow to follow

---
+++ similar_to_removesuffix_function.py
@@ -0,0 +1,5 @@
+#Prog02. removesuffix() remove the characters at the end of the string that matches the function parameter. Create a program that do the same functionality without using removesuffix() function.
+#first is to define variable
+#get the lenght of the siffix to be removed
+#condition
+#print result
