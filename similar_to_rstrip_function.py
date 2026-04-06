6a4d2f290f939dce5d89757568bb434499fde04c
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 17:52:00 +0800

    commented a flow to follow

---
+++ similar_to_rstrip_function.py
@@ -0,0 +1,9 @@
+#Prog01. rstrip() remove the space characters at the end of the string. Create a program that do the same functionality without using rstrip() function.
+#this function remove the space characters from right
+#first add a variable with stored sting statement or just a term with space at right part
+#we will start from last index to identify the location of the characters
+#use while loop to look continuously
+#then use result as variable to store the result
+#print result
+
+
