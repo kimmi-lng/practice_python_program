ce1f010afd6071a2c51de35086c76ab34f7fc8ac
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 12:11:17 +0800

    added comments or steps to follow

---
+++ remove_suffix.py
@@ -0,0 +1,5 @@
+#Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+#users input also the suffix that needs to be removed
+#variable to store the suffix
+#condition
+#print result
