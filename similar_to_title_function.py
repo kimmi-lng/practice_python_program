1b16bb5d815878b68ebf84a51ecb825a667e67d2
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 16:48:47 +0800

    followed the flow, started with the statement or the string then used the empty list to store the modified string, but the problem is they are still in the list

--- similar_to_title_function.py
+++ similar_to_title_function.py
@@ -1,4 +1,13 @@
 #Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 #we can create an empty list the store here each of the words so that they can be modified
+statement = "saNa All BALIW meOw mEow"
+#split the statement
+cut_words = statement.split()
+new_statement = []
 #condition using for loop
+for word in cut_words:
+    new_words = word[0].upper() + word[1:].lower()
+    new_statement.append(new_words)
 #print the result
+print(f"Original: {statement}")
+print(f"Result: {new_statement}")
