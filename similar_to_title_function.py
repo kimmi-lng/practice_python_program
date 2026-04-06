e131a296a36e4ec8006b047f249e6ad1692f47c8
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 16:44:23 +0800

    commented a flow to follow

---
+++ similar_to_title_function.py
@@ -0,0 +1,4 @@
+#Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+#we can create an empty list the store here each of the words so that they can be modified
+#condition using for loop
+#print the result
