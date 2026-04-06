c5fdfae7109883d86458310b7f3ba0089e4eeb71
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 12:45:46 +0800

    commented a flow to follow

---
+++ similar_to_ljust_function.py
@@ -0,0 +1,5 @@
+#Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+#time consuming if ill use the users input so maybe ill just add the fixed test
+#add or store the target width for the justify
+#set condition
+#print result
