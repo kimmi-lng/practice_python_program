8225dc970b3ac243fb466f97e2bb73e0c2c3015f
Kim Russel <kimrussel0718@gmail.com>
2026-04-06 16:29:28 +0800

    commented the flow to follow

---
+++ similar_to_capitalize_function.py
@@ -0,0 +1,5 @@
+#Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
+#this functions does two things only, it is to capitalize the first letter the swap all the other to lower case
+#first is to add a variable where the statement will be stored
+#then use if else statement to identify if the variable is valid to do the conditions, then condition follows
+#print result
