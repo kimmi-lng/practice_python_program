455b994d18221a395d4a02bf9bed14ce98be986d
Kim Russel <kimrussel0718@gmail.com>
2026-04-09 20:51:24 +0800

    commented the flow to follow

---
+++ similar_to_startswith_function.py
@@ -0,0 +1,5 @@
+#Prog05. startswith() check if the string beginning part matches the function parameter. Create a program that do the same functionality without using startswith() function.
+#will start with the given text already, no need for the users input
+#to check the prefix it will have its own variable too
+#using if else statement we can see or compare rather the given prefix to the texts
+#then print the result
