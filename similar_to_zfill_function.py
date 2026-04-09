60fa5982323a354a585f0a0eea182d12a31440b3
Kim Russel <kimrussel0718@gmail.com>
2026-04-09 21:32:18 +0800

    started with the two given data, we can identify how many zeoes to add, with that using if else statement we can compare if we need to add zero or not, then print the result

--- similar_to_zfill_function.py
+++ similar_to_zfill_function.py
@@ -2,3 +2,12 @@
 #starting  with two variable it may contain the main text or number string then the other contains the length of the target size
 #then after, using el if statement, we can identify if we will be adding zeroes in front
 #print result
+numb_string = "123"
+targ_length = 10
+#to see how many zeroes to add
+zeroes_add = targ_length - len(numb_string)
+if zeroes_add > 0:
+    result = (zeroes_add * "0") + numb_string
+else:
+    result = numb_string
+print(f"Result:{result}")
