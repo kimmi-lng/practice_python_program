63bf11f9b61eed41275b0351b29a986ea52a33bb
Kim Russel <kimrussel0718@gmail.com>
2026-04-09 20:18:04 +0800

    added a flow to follow

---
+++ similar_to_islower_function.py
@@ -0,0 +1,4 @@
+#Prog04. islower() check if all characters of the string is on lower case. Create a program that do the same functionality without using islower() function.
+#we will just again use the given statement so there is no need for users input
+#for loop so that we can check each characters if they are in lower or upper case
+#print result
