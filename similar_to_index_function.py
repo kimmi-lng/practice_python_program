0a264901b21abe83f4ae000d515de28f10c782d4
Kim Russel <kimrussel0718@gmail.com>
2026-04-09 22:17:42 +0800

    commented the flow to follow

---
+++ similar_to_index_function.py
@@ -0,0 +1,5 @@
+#Prog09. index() return the first location of the function parameter in the string. Create a program that do the same functionality without using index() function.
+#to mimic this function we will be using 2 variable at first to store the whole statement and also the word that need to be found
+#get their len so that we can identify how many index are there
+#using for loop to find the strings and the index or the range
+#then print the result
