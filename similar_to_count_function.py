1ae31977bed21f58f7d8ee3f9f34df32b919265a
Kim Russel <kimrussel0718@gmail.com>
2026-04-09 23:20:04 +0800

    started with the 2 variable the main statment where to find the 2nd variable whish is the target to look onto, using len function we indentified the length of the statment , using while look we continuously look in the main statement for the target word, then print the result

--- similar_to_count_function.py
+++ similar_to_count_function.py
@@ -1,6 +1,20 @@
 #Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
 #we will be needing counter here to mimic the count function
+occurences = 0
+counters = 0
 #start with 2 given variables the given statement where to look the 2nd variable that needs to be count
+statement = "mono naka naka ah ah naka"
+targ_text = "naka"
 #usign len function we can count the length of the statement
+statement_length = len(statement)
+targ_text_length = len(targ_text)
 #starting out loop with while loop to compare
+while counters <= statement_length - targ_text_length:
+    if statement[counters : counters + targ_text_length] == targ_text:
+        occurences += 1
+        counters += 1 # to not overlap
+    else:
+        counters += 1
 #print the result
+print(f"Original text:{statement}, and Target text:{targ_text}")
+print(f"Count:{occurences}")
