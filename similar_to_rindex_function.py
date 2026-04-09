6657818c6c0997fccdcee72115a976eb40957369
Kim Russel <kimrussel0718@gmail.com>
2026-04-09 22:57:24 +0800

    commented the flow to follow

---
+++ similar_to_rindex_function.py
@@ -0,0 +1,5 @@
+#Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
+#first defining the variable, better if its given that asking for users input
+#using len function to count how long was the text and the need to find
+#set condition
+#print result
