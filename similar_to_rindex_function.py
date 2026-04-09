968202c370b239438a841190fd9f13703bb20548
Kim Russel <kimrussel0718@gmail.com>
2026-04-09 23:06:59 +0800

    started with two given variable, the given statement and the text that need to be found its index, using len function we identify the first occurence but in backward and give the index from 0 onwards, the print the result

--- similar_to_rindex_function.py
+++ similar_to_rindex_function.py
@@ -1,5 +1,19 @@
 #Prog10. rindex() return the first location of the function parameter in the string starting from the last character. Create a program that do the same functionality without using rindex() function.
 #first defining the variable, better if its given that asking for users input
+statement = "saNa All BALIW meOw mEow"
+targ_index = "All"
 #using len function to count how long was the text and the need to find
+statement_length = len(statement)
+targ_index_length = len(targ_index)
 #set condition
+found_index = -1
+position = statement_length - targ_index_length
+for i in range(position, -1, -1):
+    if statement[i :i + targ_index_length] == targ_index:
+        found_index = i
+        break
 #print result
+if found_index != -1:
+    print(f"Found index: {found_index}")
+else:
+    print(f"Could not find index: {targ_index}")
