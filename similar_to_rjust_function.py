49d377c9ccc749fd02ce4f508030669738de056a
Kim Russel <kimrussel0718@gmail.com>
2026-04-09 21:12:59 +0800

    commented the flow to follow

---
+++ similar_to_rjust_function.py
@@ -0,0 +1,4 @@
+#Prog06. rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using rjust() function.
+#we need 2 things here the main text and the variable which define how long will be the text to be justified
+#text and length will be given
+#using if else statement we can identify how many space char to add and if we will add
