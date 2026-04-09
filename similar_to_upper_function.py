ef4c6827f9f89a93bda4d49b6e3fa2fc22cbdf41
Kim Russel <kimrussel0718@gmail.com>
2026-04-09 19:44:47 +0800

    started with the statment or the text itself then added an empty strin in a variable so that i can add the char if or if has no changes thats with the use of for loop and if else statement then lastly printed the result

--- similar_to_upper_function.py
+++ similar_to_upper_function.py
@@ -1,5 +1,15 @@
 #Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
 #add variable maybe so that we can just change all lowered case into upper case without asking for users inputs
+statement = "saNa All BALIW meOw mEow"
 #add an empty variable so that it can hold the changed char
+new_case = ""
 #a loop to check each char
+for char in statement:
+    if 'a' <= char <= 'z':
+        upp_case = chr(ord(char) - 32)
+        new_case += upp_case
+    else:
+        new_case += char
 #then print result
+print(f"Original: {statement}")
+print(f"Result: {new_case}")
