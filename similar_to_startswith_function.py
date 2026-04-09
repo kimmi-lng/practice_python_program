98d95dccbd7669755d257352266387c546fa4b28
Kim Russel <kimrussel0718@gmail.com>
2026-04-09 20:57:23 +0800

    started with given statement and given prefix tomimic what the startswith fuction do, using el if statement we can compare each character of the prefix to the first part of the text or the statement wherein we can decide for the result

--- similar_to_startswith_function.py
+++ similar_to_startswith_function.py
@@ -3,3 +3,16 @@
 #to check the prefix it will have its own variable too
 #using if else statement we can see or compare rather the given prefix to the texts
 #then print the result
+statement = "saNa All BALIW meOw mEow"
+starts_with = "saNa"
+#given the prefix we can use len function to find its index
+count_prefix = len(starts_with)
+
+if count_prefix == 0:
+    decision = True
+elif count_prefix > len(statement):
+    decision = False
+else:
+    decision = statement[:count_prefix] == starts_with
+
+print(f"Decision: {decision}") #a boolean is the result
