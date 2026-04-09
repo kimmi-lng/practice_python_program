44a107f6f65b3c989cb86ac21da5d09a2bc33e90
Kim Russel <kimrussel0718@gmail.com>
2026-04-09 20:40:58 +0800

    started again with the given statement or the text to wee if there is lower cased in it then using for loop i identify if the text has lower case, if it has it will results false, a boollean

--- similar_to_islower_function.py
+++ similar_to_islower_function.py
@@ -2,3 +2,22 @@
 #we will just again use the given statement so there is no need for users input
 #for loop so that we can check each characters if they are in lower or upper case
 #print result
+statement = "saNa All BALIW meOw mEow"
+
+has_upper = False # we can assume that they are false
+has_lower = False
+
+for char in statement:
+
+    if 'A' <= char <= 'Z':
+        has_upper = True
+        break
+    if 'a' <= char <= 'z':
+        has_lower = True
+
+if has_lower and not has_upper:
+    decision = True
+else:
+    decision = False
+
+print(f"{decision}")
