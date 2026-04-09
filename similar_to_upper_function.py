c1df8cc05cffb6500994d94d3338647412cafbb2
Kim Russel <kimrussel0718@gmail.com>
2026-04-09 19:31:30 +0800

    commented a flow to follow

---
+++ similar_to_upper_function.py
@@ -0,0 +1,5 @@
+#Prog03. upper() converts all characters of the string into upper case. Create a program that do the same functionality without using upper() function.
+#add variable maybe so that we can just change all lowered case into upper case without asking for users inputs
+#add an empty variable so that it can hold the changed char
+#a loop to check each char
+#then print result
