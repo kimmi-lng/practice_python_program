b26cb6d758dc8c9220d55b409ecd2bcedb7c48f9
Kim Russel <kimrussel0718@gmail.com>
2026-04-09 21:27:45 +0800

    commented a flow to follow

---
+++ similar_to_zfill_function.py
@@ -0,0 +1,4 @@
+#Prog07. zfill() add zero characters at the beginning of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using zfill() function.
+#starting  with two variable it may contain the main text or number string then the other contains the length of the target size
+#then after, using el if statement, we can identify if we will be adding zeroes in front
+#print result
