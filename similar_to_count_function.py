572e3abf5d54a9bb9ddd971513f65478f7067bf7
Kim Russel <kimrussel0718@gmail.com>
2026-04-09 23:11:08 +0800

    commented the flow to follow

---
+++ similar_to_count_function.py
@@ -0,0 +1,6 @@
+#Prog08. count() return how many time the function parameter appear in the string. Create a program that do the same functionality without using count() function.
+#we will be needing counter here to mimic the count function
+#start with 2 given variables the given statement where to look the 2nd variable that needs to be count
+#usign len function we can count the length of the statement
+#starting out loop with while loop to compare
+#print the result
